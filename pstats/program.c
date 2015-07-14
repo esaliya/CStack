ab6c07fb6e132d746c962a6f37c310b2eed9e73a
Saliya Ekanayake <sekanaya@cs.indiana.edu>
2015-07-14 12:42:16 -0400

    fixing program.c

--- pstats/program.c
+++ pstats/program.c
@@ -13,19 +13,19 @@ int main(int argc, char* argvi[])
 	  cpu_set_t my_set;
     int ret;
     CPU_ZERO(&my_set);
-		CPU_SET(1, &my_set);
-		CPU_SET(2, &my_set);
+//		CPU_SET(1, &my_set);
+//		CPU_SET(2, &my_set);
 
-    ret = sched_setaffinity(0, 1, &my_set);
+/*    ret = sched_setaffinity(0, sizeof(my_set), &my_set);
 	  if (ret < 0)
 	  {
 		  printf("error");
-	  }
+	  }*/
 	  
-	  ret = sched_getaffinity(0, 1, &my_set);
-		printf("pid %d affinity includes CPUs ... ", pid);
+	  ret = sched_getaffinity(0, sizeof(my_set), &my_set);
+		printf("pid %d affinity includes %d CPUs ... ", pid, CPU_COUNT(&my_set));
 		int j;
-		for (j = 0; j < 4; ++j)
+		for (j = 0; j < CPU_SETSIZE; ++j)
 		{
 			if (CPU_ISSET(j, &my_set))
 			{
@@ -33,9 +33,8 @@ int main(int argc, char* argvi[])
 			}
 
 		}
-		//printf ("pid %d aiffinity %lx\n", pid, my_set);
+		printf("\n");
 
-		/*
 	  long x = 0;
 	  long i;	
 	  while (i < LONG_MAX)
@@ -43,9 +42,8 @@ int main(int argc, char* argvi[])
 	  	x += i;
 	  	++i;
 	  }
-	  printf("%ld\n",x);*/
+	  printf("%ld\n",x);
 
-		printf("hello");
 	  return 0;
 }
 
