19efbc87d0b98f30c2daa2571aa95c71f2ba0d75
Saliya Ekanayake <sekanaya@indiana.edu>
2016-06-28 23:27:51 -0400

    adding cpu set

--- pstats/program.c
+++ pstats/program.c
@@ -10,40 +10,39 @@ int main(int argc, char* argvi[])
   	pid_t pid = getpid();
 
 		
-	  cpu_set_t my_set;
+	cpu_set_t my_set;
     int ret;
     CPU_ZERO(&my_set);
-//		CPU_SET(1, &my_set);
-//		CPU_SET(2, &my_set);
+	CPU_SET(1, &my_set);
+	CPU_SET(2, &my_set);
 
-/*    ret = sched_setaffinity(0, sizeof(my_set), &my_set);
-	  if (ret < 0)
-	  {
-		  printf("error");
-	  }*/
+    ret = sched_setaffinity(0, sizeof(my_set), &my_set);
+    if (ret < 0)
+    {
+		printf("error");
+	}
 	  
-	  ret = sched_getaffinity(0, sizeof(my_set), &my_set);
-		printf("pid %d affinity includes %d CPUs ... ", pid, CPU_COUNT(&my_set));
-		int j;
-		for (j = 0; j < CPU_SETSIZE; ++j)
+	ret = sched_getaffinity(0, sizeof(my_set), &my_set);
+	printf("pid %d affinity includes %d CPUs ... ", pid, CPU_COUNT(&my_set));
+	int j;
+	for (j = 0; j < CPU_SETSIZE; ++j)
+	{
+		if (CPU_ISSET(j, &my_set))
 		{
-			if (CPU_ISSET(j, &my_set))
-			{
-				printf("%d ",j);
-			}
-
+			printf("%d ",j);
 		}
-		printf("\n");
+	}
+	printf("**\n");
 
-	  long x = 0;
-	  long i;	
-	  while (i < LONG_MAX)
-	  {
-	  	x += i;
-	  	++i;
-	  }
-	  printf("%ld\n",x);
+	long x = 0;
+	long i = 0;	
+	while (i < LONG_MAX)
+	{
+		x += i;
+	 	++i;
+	}
+	printf("%ld\n",x);
 
-	  return 0;
+	return 0;
 }
 
